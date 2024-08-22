df64117547165d0b35d61fb915cae292aeceb51e
Max Frolenko <maximzec3200@gmail.com>
2024-08-22 21:45:21 +0300

    Победил ошибку парсинга RequestBody Добавил логирование

--- src/main/java/ru/neiropulse/backend/controller/CategoryController.java
+++ src/main/java/ru/neiropulse/backend/controller/CategoryController.java
@@ -1,16 +1,16 @@
 package ru.neiropulse.backend.controller;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
-import jakarta.validation.Valid;
 import ru.neiropulse.backend.dto.CategoryDto;
 import ru.neiropulse.backend.service.CategoryService;
 
@@ -18,15 +18,18 @@ import ru.neiropulse.backend.service.CategoryService;
 @RequestMapping("/api/v1/category")
 @RestController
 @AllArgsConstructor
+@Slf4j
 public class CategoryController {
 
     @Autowired
     private final CategoryService service;
 
-
     @PostMapping("")
-    public ResponseEntity<CategoryDto> create(@RequestBody CategoryDto categoryDto) {
-        return ResponseEntity.ok(categoryDto);
+    public ResponseEntity<CategoryDto> create(@RequestBody CategoryDto dto) {
+        log.info("Requested to create category");
+        CategoryDto savedCategory = service.createCategory(dto);
+        log.info("Category successfully created");
+        return ResponseEntity.ok(savedCategory);
     }
 
 }
